c789e834737fb8b43f11761be5332da35cb96aef
alibiss <silvioalbi@outlook.com>
2023-03-11 11:11:27 +0100

    Update job flag method

--- src/actions-html-builder.js
+++ src/actions-html-builder.js
@@ -16,7 +16,7 @@ new Promise(parsedDb => {
         output += `<div class="${category}">`;
         const jobs = Object.values(database[category]);
         jobs.forEach(job => { // PLD, WAR, DRK, etc..
-            output += `<div class="d-flex flex-wrap gap-3 ${job.code}">`;
+            output += `<div class="d-flex flex-wrap gap-3" data-job="${job.code}">`;
             const modes = Object.keys(job.actions);
             modes.forEach(mode => { // pve, pvp
                 output += `<div class="d-flex flex-wrap gap-1 ${mode}">`;
