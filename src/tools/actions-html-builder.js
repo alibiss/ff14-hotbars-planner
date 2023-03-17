6719e5bd5d5b7fa6969369f46774647428b5bd35
alibiss <silvioalbi@outlook.com>
2023-03-17 15:16:00 +0100

    Refactor template and add job mode toggle

--- src/tools/actions-html-builder.js
+++ src/tools/actions-html-builder.js
@@ -16,10 +16,10 @@ new Promise(parsedDb => {
         output += `<div class="${category}">`;
         const jobs = Object.values(database[category]);
         jobs.forEach(job => { // PLD, WAR, DRK, etc..
-            output += `<div class="d-flex flex-wrap gap-3" data-job="${job.code}">`;
+            output += `<div data-job="${job.code}">`;
             const modes = Object.keys(job.actions);
             modes.forEach(mode => { // pve, pvp
-                output += `<div class="d-flex flex-wrap gap-1 ${mode}">`;
+                output += `<div class="${mode}">`;
                 const types = Object.keys(job.actions[mode]);
                 types.forEach(type => { // jobActions, roleActions, etc..
                     // output += `<div class="${type}">`;
